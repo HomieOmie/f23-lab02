42038d28764a3142eed951777aabe5f7e9cf7da2
Om Patel <opatel@andrew.cmu.edu>
2023-09-05 19:28:59 -0400

    Added renders for circle and square to check functionalit

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,16 @@
 import { newRenderer } from './renderer.js'
+import { newCircle } from './shapes/circle.js'
 import { newRectangle } from './shapes/rectangle.js'
+import { newSquare } from './shapes/square.js'
 
 const rectangle: Shape = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw()
+const circle: Shape = newCircle(3)
+const square: Shape = newSquare(2)
+
+const rectangleRenderer = newRenderer(rectangle)
+const circleRenderer = newRenderer(circle)
+const squareRenderer = newRenderer(square)
+
+rectangleRenderer.draw()
+circleRenderer.draw()
+squareRenderer.draw()
