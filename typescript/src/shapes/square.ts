288d5ad7fdefa66e07804e2cda8b9dba7a940c3a
Om Patel <opatel@andrew.cmu.edu>
2023-09-05 19:21:26 -0400

    Refactored code for square.ts to implement Shape interface

--- typescript/src/shapes/square.ts
+++ typescript/src/shapes/square.ts
@@ -1,10 +1,13 @@
-function square (sideLen: number): Shape {
-  return {
-    sideLen,
-    computeArea: function (): number {
-      return sideLen * sideLen
-    }
+class Square implements Shape {
+  constructor(private sideLen: number) {}
+
+  computeArea(): number {
+      return this.sideLen * this.sideLen
   }
 }
 
-export { square }
+function newSquare(sideLen: number) {
+  return new Square(sideLen)
+}
+
+export { newSquare }
